df2d3726d78d92e4bf50aece13c13a5f69f67b44
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-09-11 18:13:54 +0300

    Add DocumentResponse DTO to storage-service

---
+++ services/storage-service/src/main/java/com/github/vadimmiheev/vectordocs/storageservice/dto/DocumentResponse.java
@@ -0,0 +1,19 @@
+package com.github.vadimmiheev.vectordocs.storageservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class DocumentResponse {
+    private String id;
+    private String name;
+    private long size;
+    private String userId;
+    private String contentType;
+    private Instant createdAt;
+}
