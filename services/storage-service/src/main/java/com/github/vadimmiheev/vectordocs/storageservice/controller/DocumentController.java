54b7bee111d7572def149020211eb1972821dca4
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-09-12 14:44:06 +0300

    Update DocumentController to use `X-User-Id` from headers instead of `userId` query parameter

--- services/storage-service/src/main/java/com/github/vadimmiheev/vectordocs/storageservice/controller/DocumentController.java
+++ services/storage-service/src/main/java/com/github/vadimmiheev/vectordocs/storageservice/controller/DocumentController.java
@@ -25,19 +25,19 @@ public class DocumentController {
     private final DocumentStorageService storageService;
 
     @GetMapping
-    public ResponseEntity<List<DocumentResponse>> list(@RequestParam("userId") String userId) {
+    public ResponseEntity<List<DocumentResponse>> list(@RequestHeader("X-User-Id") String userId) {
         return ResponseEntity.ok(storageService.getUserDocuments(userId));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<DocumentResponse> getById(@PathVariable("id") String id,
-                                                    @RequestParam("userId") String userId) {
+                                                    @RequestHeader("X-User-Id") String userId) {
         return ResponseEntity.ok(storageService.getById(userId, id));
     }
 
     @GetMapping("/{id}/download")
     public ResponseEntity<Resource> download(@PathVariable("id") String id,
-                                             @RequestParam("userId") String userId) {
+                                             @RequestHeader("X-User-Id") String userId) {
         Document doc = storageService.getDocument(userId, id);
         Path path = Paths.get(doc.getPath());
         PathResource resource = new PathResource(path);
@@ -53,14 +53,14 @@ public class DocumentController {
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<DocumentResponse> upload(@RequestParam("userId") String userId,
+    public ResponseEntity<DocumentResponse> upload(@RequestHeader("X-User-Id") String userId,
                                                    @RequestPart("file") MultipartFile file) {
         return ResponseEntity.ok(storageService.save(userId, file));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") String id,
-                                       @RequestParam("userId") String userId) {
+                                       @RequestHeader("X-User-Id") String userId) {
         long result = storageService.delete(userId, id);
         if (result == 0) {
             return ResponseEntity.internalServerError().build();
