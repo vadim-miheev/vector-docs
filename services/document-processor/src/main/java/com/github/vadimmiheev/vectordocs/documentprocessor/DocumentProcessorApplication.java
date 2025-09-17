c342ab512721ee6fcd955357d2e57c1090b0cd3c
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-09-17 18:15:40 +0300

    Enable Kafka support in DocumentProcessorApplication

--- services/document-processor/src/main/java/com/github/vadimmiheev/vectordocs/documentprocessor/DocumentProcessorApplication.java
+++ services/document-processor/src/main/java/com/github/vadimmiheev/vectordocs/documentprocessor/DocumentProcessorApplication.java
@@ -2,7 +2,9 @@ package com.github.vadimmiheev.vectordocs.documentprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class DocumentProcessorApplication {
     public static void main(String[] args) {
