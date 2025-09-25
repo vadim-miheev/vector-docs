d8e953669e8cec66e9b4858f2074877152c83e2e
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-09-25 17:45:44 +0300

    Enable Kafka support in SearchServiceApplication

--- services/search-service/src/main/java/com/github/vadimmiheev/vectordocs/searchservice/SearchServiceApplication.java
+++ services/search-service/src/main/java/com/github/vadimmiheev/vectordocs/searchservice/SearchServiceApplication.java
@@ -2,7 +2,9 @@ package com.github.vadimmiheev.vectordocs.searchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class SearchServiceApplication {
     public static void main(String[] args) {
