6092eef1dd930404f04908d3d8d1fbcaa40c67fe
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-10-10 12:43:08 +0300

    Add TailwindCSS setup to React app

---
+++ frontend/react-app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/*.{js,jsx,ts,tsx}",
+    "./src/components/*.{js,jsx,ts,tsx}",
+    "./src/pages/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
