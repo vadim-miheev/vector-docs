adc320272ffc4e9a408c7bfdabf2d3e6fb6233cf
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-10-25 00:25:30 +0300

    Enable Scheduling

--- services/document-processor/src/main/java/com/github/vadimmiheev/vectordocs/documentprocessor/DocumentProcessorApplication.java
+++ services/document-processor/src/main/java/com/github/vadimmiheev/vectordocs/documentprocessor/DocumentProcessorApplication.java
@@ -3,9 +3,11 @@ package com.github.vadimmiheev.vectordocs.documentprocessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableKafka
 @SpringBootApplication
+@EnableScheduling
 public class DocumentProcessorApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocumentProcessorApplication.class, args);
