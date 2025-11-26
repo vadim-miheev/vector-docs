268a902ee0845d6b586427793889560b669a209d
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-11-26 19:00:33 +0300

    Add rendering state and spinner animation to indicate ongoing rendering

--- frontend/react-app/src/pages/SearchPage.jsx
+++ frontend/react-app/src/pages/SearchPage.jsx
@@ -37,6 +37,7 @@ export default function SearchPage() {
   });
   const [input, setInput] = useState('');
   const [isSending, setIsSending] = useState(false);
+  const [isRendering, setIsRendering] = useState(false);
   const currentRequestIDRef = useRef('');
   const currentAgentMessageRef = useRef({});
   const {addMessage} = useNotifications();
@@ -118,10 +119,12 @@ export default function SearchPage() {
       if (payload?.requestId !== currentId) return;
 
       setIsSending(false);
+      setIsRendering(true);
 
       if (payload?.complete === true) {
         currentAgentMessageRef.current = {};
         currentRequestIDRef.current = '';
+        setIsRendering(false);
         return;
       }
 
@@ -310,6 +313,17 @@ export default function SearchPage() {
                     </div>
                   )}
                 </div>
+                {isRendering && currentAgentMessageRef.current === m && (
+                  <div className="w-full max-w-3xl flex justify-end -translate-x-1 -translate-y-5">
+                    <svg className="animate-spin h-4 w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                              strokeWidth="4"></circle>
+                      <path className="opacity-75" fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                  </div>
+                )}
               </div>
             ))}
             {isSending && (
