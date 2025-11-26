67d879bdaabdddf0b5197412760774fbfd170e03
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-11-26 21:32:30 +0300

    Spinner animation to indicate ongoing rendering

--- frontend/react-app/src/pages/SearchPage.jsx
+++ frontend/react-app/src/pages/SearchPage.jsx
@@ -290,7 +290,7 @@ export default function SearchPage() {
                 <div
                   className={
                     m.role === 'agent'
-                      ? 'inline-block max-w-3xl rounded-lg bg-neutral-200 border border-gray-400 px-4 py-3 text-black'
+                      ? 'inline-block max-w-3xl rounded-lg bg-neutral-200 border border-gray-400 px-4 py-3 text-black relative'
                       : 'inline-block max-w-3xl rounded-lg border border-gray-400 px-4 py-3 text-black text-left'
                   }
                 >
@@ -312,18 +312,19 @@ export default function SearchPage() {
                       </ul>
                     </div>
                   )}
+                  {isRendering && currentAgentMessageRef.current === m && (
+                    <div className="absolute left-0 -bottom-6">
+                      <svg className="animate-spin h-4 w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
+                           viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                      </svg>
+                    </div>
+                  )}
                 </div>
-                {isRendering && currentAgentMessageRef.current === m && (
-                  <div className="w-full max-w-3xl flex justify-end -translate-x-1 -translate-y-5">
-                    <svg className="animate-spin h-4 w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
-                         viewBox="0 0 24 24">
-                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                              strokeWidth="4"></circle>
-                      <path className="opacity-75" fill="currentColor"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
-                  </div>
-                )}
+
               </div>
             ))}
             {isSending && (
