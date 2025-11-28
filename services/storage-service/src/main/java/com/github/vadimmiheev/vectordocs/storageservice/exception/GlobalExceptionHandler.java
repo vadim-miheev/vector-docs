03095feff01dd85d9a39f693252fa6eff0d489fa
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-11-28 16:54:27 +0300

    Storage service: max upload file limit update; error handler for UI;

--- services/storage-service/src/main/java/com/github/vadimmiheev/vectordocs/storageservice/exception/GlobalExceptionHandler.java
+++ services/storage-service/src/main/java/com/github/vadimmiheev/vectordocs/storageservice/exception/GlobalExceptionHandler.java
@@ -1,18 +1,25 @@
 package com.github.vadimmiheev.vectordocs.storageservice.exception;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    @Value("${spring.servlet.multipart.max-file-size:50MB}")
+    private String maxFileSize;
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleNotFound(ResourceNotFoundException ex) {
@@ -42,6 +49,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, Object>> handleGeneric(Exception ex) {
         log.error("Unexpected error occurred", ex);
         return build(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException ignoredExc) {
+        return ResponseEntity.badRequest().body("File size exceeds the maximum allowed limit " + maxFileSize);
     }
 
     private ResponseEntity<Map<String, Object>> build(HttpStatus status, String message) {
