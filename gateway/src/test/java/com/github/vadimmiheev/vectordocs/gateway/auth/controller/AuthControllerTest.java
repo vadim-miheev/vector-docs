e52efdefe7c235c38ebf34368ed949241ff5fc1e
Vadim Miheev <vadim@Lenovo-81Y600.localdomain>
2025-12-08 11:01:01 +0300

    Replace MockBean with MockitoBean in AuthControllerTest

--- gateway/src/test/java/com/github/vadimmiheev/vectordocs/gateway/auth/controller/AuthControllerTest.java
+++ gateway/src/test/java/com/github/vadimmiheev/vectordocs/gateway/auth/controller/AuthControllerTest.java
@@ -5,7 +5,7 @@ import com.github.vadimmiheev.vectordocs.gateway.auth.service.AuthService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -18,7 +18,7 @@ class AuthControllerTest {
     @Autowired
     private WebTestClient webTestClient;
 
-    @MockBean
+    @MockitoBean
     private AuthService authService;
 
     @Test
